af4710f713f3ce364ff4a75226206a9f525a7c1b
hyemigwak <goadl92@gmail.com>
2022-05-02 13:31:01 +0900

    병합

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { QueryClient ,QueryClientProvider } from 'react-query'
 import Main from "../pages/Main";
 import SignUp from "../pages/SignUp";
 import WriteDiary from "../pages/WriteDiary";
@@ -15,20 +16,22 @@ import NotFound from "../pages/NotFound";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/post" element={<WriteDiary />} />
-            <Route path="/diarylist" element={<DiaryList />} />
-            <Route path="/select" element={<SelectMode />} />
-            <Route path="/diary/:id" element={<DiaryDetail />} />
-            <Route path="/chat/:id" element={<ChatDetail />} />
-            <Route path="/chatlist" element={<ChatList />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/mypage/temp" element={<MyTempPage />} />
-            <Route path="/alert" element={<Notifications />} />
-            <Route path={"*"} element={<NotFound />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/post" element={<WriteDiary />} />
+                <Route path="/diarylist" element={<DiaryList />} />
+                <Route path="/select" element={<SelectMode />} />
+                <Route path="/diary/:id" element={<DiaryDetail />} />
+                <Route path="/chat/:id" element={<ChatDetail />} />
+                <Route path="/chatlist" element={<ChatList />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/mypage/temp" element={<MyTempPage />} />
+                <Route path="/alert" element={<Notifications />} />
+                <Route path={"*"} element={<NotFound />} />
+            </Routes>
+        </QueryClientProvider>
     );
 }
 
