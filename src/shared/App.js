9bf96139c1738522598a3cc774fccfd442cef301
lee_chun <lcg5320@gmail.com>
2022-05-02 13:00:50 +0900

    feat: 마이페이지 분기

--- src/shared/App.js
+++ src/shared/App.js
@@ -9,25 +9,27 @@ import DiaryDetail from "../pages/DiaryDetail";
 import ChatDetail from "../pages/ChatDetail";
 import ChatList from "../pages/ChatList";
 import MyPage from "../pages/MyPage";
+import MyTempPage from "../pages/MyTempPage";
 import { Notifications } from "../components/common";
 import NotFound from "../pages/NotFound";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/post" element={<WriteDiary />} />
-      <Route path="/diarylist" element={<DiaryList />} />
-      <Route path="/select" element={<SelectMode />} />
-      <Route path="/diary/:id" element={<DiaryDetail />} />
-      <Route path="/chat/:id" element={<ChatDetail />} />
-      <Route path="/chatlist" element={<ChatList />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/alert" element={<Notifications />} />
-      <Route path={"*"} element={<NotFound />}/>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/post" element={<WriteDiary />} />
+            <Route path="/diarylist" element={<DiaryList />} />
+            <Route path="/select" element={<SelectMode />} />
+            <Route path="/diary/:id" element={<DiaryDetail />} />
+            <Route path="/chat/:id" element={<ChatDetail />} />
+            <Route path="/chatlist" element={<ChatList />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/mypage/temp" element={<MyTempPage />} />
+            <Route path="/alert" element={<Notifications />} />
+            <Route path={"*"} element={<NotFound />} />
+        </Routes>
+    );
 }
 
 export default App;
