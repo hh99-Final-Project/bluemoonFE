d42b6753dfc7835d911f96af60ac3b4547e0d1ff
lee_chun <lcg5320@gmail.com>
2022-05-23 18:37:53 +0900

    feat: 알람 무한스크롤 구현

--- src/components/common/Notifications.js
+++ src/components/common/Notifications.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Notice from "./Notice";
 import Modal from "react-modal";
-import {closeButton, mobAlertCloseBtn} from "../../static/images/resources";
+import { closeButton, mobAlertCloseBtn } from "../../static/images/resources";
 import { getAlertList } from "../../redux/modules/commonSlice";
 import { diaryApi } from "../../apis/diaryApi";
 import { isMobile } from "react-device-detect";
@@ -23,6 +23,28 @@ function Notifications(props) {
     const [page, setPage] = useState(1);
     const [hasNext, setHasNext] = useState(null);
 
+    console.log(hasNext);
+
+    // 무한스크롤
+    const InfinityScroll = _.throttle((e) => {
+        if (e.target.scrollHeight - (e.target.scrollTop + e.target.clientHeight) <= 200 && hasNext === true) {
+            diaryApi
+                .getCommentAlertList(page)
+                .then((response) => {
+                    setCommentAlertList([...commentAlertList, ...response.data]);
+                    setIsLoading(false);
+                    if (response.data.length < 10) {
+                        setHasNext(false);
+                    } else {
+                        setHasNext(true);
+                    }
+                    setPage(page + 1);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, 300);
 
     useEffect(() => {
         diaryApi
@@ -70,19 +92,19 @@ function Notifications(props) {
                     content: {
                         position: "absolute",
                         zIndex: 10,
-                        top: (isMobile || isMobileQuery) ? "53px" : AlertTabRef.current.getBoundingClientRect().top + 38,
-                        left: (isMobile || isMobileQuery) ? "50%" : AlertTabRef.current.getBoundingClientRect().left,
+                        top: isMobile || isMobileQuery ? "53px" : AlertTabRef.current.getBoundingClientRect().top + 38,
+                        left: isMobile || isMobileQuery ? "50%" : AlertTabRef.current.getBoundingClientRect().left,
                         right: "auto",
                         bottom: "auto",
                         transform: "translate(-50%, 0)",
-                        width: (isMobile || isMobileQuery) ? "320px" : "248px",
-                        height: (isMobile || isMobileQuery) ? "calc(100% - 100px)" : "586px",
+                        width: isMobile || isMobileQuery ? "320px" : "248px",
+                        height: isMobile || isMobileQuery ? "calc(100% - 100px)" : "586px",
                         border: "none",
                         boxSizing: "border-box",
                         background: "rgba(198, 211, 236, 0.8)",
                         borderRadius: "10px",
                         outline: "none",
-                        padding: (isMobile || isMobileQuery) ? "0 7px 18px 0" : "0 7px 26px 0",
+                        padding: isMobile || isMobileQuery ? "0 7px 18px 0" : "0 7px 26px 0",
                         overflowY: "hidden",
                     },
                 }}
@@ -90,25 +112,21 @@ function Notifications(props) {
                 <NotiHeader>
                     <Title>알림</Title>
                     <CloseButton onClick={closeModal}>
-                        { (isMobile || isMobileQuery) ?
-                            <img src={mobAlertCloseBtn} alt={"mobAlertCloseBtn"}/>
-                            :
+                        {isMobile || isMobileQuery ? (
+                            <img src={mobAlertCloseBtn} alt={"mobAlertCloseBtn"} />
+                        ) : (
                             <img src={closeButton} alt={"close_alert"} />
-                        }
-
-
+                        )}
                     </CloseButton>
                 </NotiHeader>
-                <Content length={commentAlertList.length}>
-                    {commentAlertList.length > 0 ?
+                <Content length={commentAlertList.length} ref={InfinityScrollref} onScroll={InfinityScroll}>
+                    {commentAlertList.length > 0 ? (
                         commentAlertList.map((alert) => {
                             return <Notice key={alert.messageId} alert={alert} />;
                         })
-                        :
-                        <NoContent>
-                            아직 도착한 알림이 없어요:)
-                        </NoContent>
-                    }
+                    ) : (
+                        <NoContent>아직 도착한 알림이 없어요:)</NoContent>
+                    )}
                 </Content>
             </Modal>
         </div>
@@ -128,12 +146,12 @@ const Title = styled.div`
     color: #08105d;
     margin: 18px 0 18px 14px;
 
-  @media only screen and (max-width: 420px) {
-    font-size: 20px;
-    line-height: 25px;
-    color: #53648B;
-    margin-left: 19px;
-  }
+    @media only screen and (max-width: 420px) {
+        font-size: 20px;
+        line-height: 25px;
+        color: #53648b;
+        margin-left: 19px;
+    }
 `;
 
 const CloseButton = styled.div`
@@ -149,9 +167,9 @@ const Content = styled.div`
     padding-right: 7px;
     box-sizing: border-box;
 
-  @media only screen and (max-width: 420px) {
-    height: calc(100% - 59px);
-  }
+    @media only screen and (max-width: 420px) {
+        height: calc(100% - 59px);
+    }
 
     &::-webkit-scrollbar {
         width: 6px;
@@ -160,7 +178,6 @@ const Content = styled.div`
     &::-webkit-scrollbar-thumb {
         background-color: #d3d3d3;
         border-radius: 5px;
-      
     }
 
     &::-webkit-scrollbar-track {
@@ -169,9 +186,9 @@ const Content = styled.div`
     }
 `;
 const NoContent = styled.div`
-  font-size: 15px;
-  line-height: 19px;
-  color: #08105D;
-  text-align: center;
-  margin-top: 223px;
+    font-size: 15px;
+    line-height: 19px;
+    color: #08105d;
+    text-align: center;
+    margin-top: 223px;
 `;
