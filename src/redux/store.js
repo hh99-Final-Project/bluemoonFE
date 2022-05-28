f0c66b548a5d054a870fe23c066f90dca16bf19c
hyemigwak <goadl92@gmail.com>
2022-05-28 18:05:58 +0900

    fix: 아니 왜 logger가 안사라지지...

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,7 @@ import logger from "redux-logger";
 
 export const store = configureStore({
     reducer,
-    middleware: process.env.NODE_ENV === "development" ? (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false,
-    }).concat(logger) : (getDefaultMiddleware) => getDefaultMiddleware({
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),
     devTools: process.env.NODE_ENV === "development"
