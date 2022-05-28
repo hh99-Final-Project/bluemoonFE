edd7428a1b32f6d845d2e4a15ed186b5d0b7e58d
hyemigwak <goadl92@gmail.com>
2022-05-28 15:14:54 +0900

    refactor: 불필요한 코드 삭제

--- src/pages/WriteDiary.js
+++ src/pages/WriteDiary.js
@@ -330,44 +330,6 @@ const WriteArea = styled.div`
     }
 `;
 
-const PostText = styled.input`
-    width: 876px;
-    height: 47px;
-    background-color: #959ebe;
-    outline: none;
-    border: none;
-    padding: 13px 27px;
-    border-radius: 10px;
-    box-sizing: border-box;
-    margin-bottom: 10px;
-    font-size: 18px;
-    line-height: 23px;
-
-    @media only screen and (max-width: 420px) {
-        width: 320px;
-        height: 43px;
-        border: 1px solid #6b6b6b;
-        padding: 13px 20px;
-        margin-bottom: 16px;
-        font-size: 14px;
-        line-height: 18px;
-        color: #08105d;
-        background-color: #b1bbd6;
-    }
-
-    ::placeholder {
-        font-size: 18px;
-        line-height: 23px;
-        color: rgba(8, 16, 93, 0.5);
-
-        @media only screen and (max-width: 420px) {
-            font-size: 15px;
-            line-height: 18px;
-            color: rgba(227, 229, 255, 0.5);
-        }
-    }
-`;
-
 const PostContainer = styled.div`
     @media only screen and (max-width: 420px) {
         position: relative;
